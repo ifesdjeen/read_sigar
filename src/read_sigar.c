bd30b608862959c09c323482d9e53b1f7e87becb
Alex Petrov <alexp@coffeenco.de>
2014-12-01 23:26:39 +0100

    It compiles, yay

--- src/read_sigar.c
+++ src/read_sigar.c
@@ -12,17 +12,67 @@
 
 #include "sigar.h"
 
+sigar_t *sigar;
 
-static int
-sigar_config (oconfig_item_t *ci)
+static int init (void)
 {
+  sigar_open(&sigar);
+
   return (0);
 }
 
+static void submit_value (const char *type, const char *type_instance, value_t value)
+{
+	value_t values[1];
+	value_list_t vl = VALUE_LIST_INIT;
+
+	memcpy(&values[0], &value, sizeof(value));
+
+	vl.values = values;
+	vl.values_len = 1;
+
+	sstrncpy (vl.host,          hostname_g,    sizeof (vl.host));
+	sstrncpy (vl.plugin,        "read_sigar",  sizeof (vl.plugin));
+	sstrncpy (vl.type,          type,          sizeof (vl.type));
+  sstrncpy (vl.type_instance, type_instance, sizeof (vl.type_instance));
+
+	plugin_dispatch_values (&vl);
+}
+
+static void
+submit_percent(const char *type_instance, gauge_t percent)
+{
+	value_t value;
+
+	value.gauge = percent;
+	submit_value ("percent", type_instance, value);
+}
+
+static void
+submit_derive(const char *type_instance, derive_t derive)
+{
+	value_t value;
+
+	value.derive = derive;
+	submit_value ("derive", type_instancetype, value);
+}
+
+static int sigar_read (void)
+{
+  sigar_cpu_t cpu;
+
+  if (SIGAR_OK == sigar_cpu_get(sigar, &cpu)) {
+    submit_percent("system.cpu.user",  cpu.user);
+    submit_percent("system.cpu.sys",   cpu.sys);
+    submit_percent("system.cpu.nice",  cpu.nice);
+    submit_percent("system.cpu.idle",  cpu.idle);
+  }
+}
 void
 module_register (void)
 {
-  plugin_register_complex_config ("read_sigar", sigar_config);
+	plugin_register_init ("read_sigar", init);
+	plugin_register_read ("read_sigar", sigar_read);
 }
 
 
