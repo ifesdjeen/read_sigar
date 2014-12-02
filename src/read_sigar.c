1ad469cbc1122b3c072f03aa376a2590985f82d2
Alex Petrov <alexp@coffeenco.de>
2014-12-02 19:24:13 +0100

    Encode type into value

--- src/read_sigar.c
+++ src/read_sigar.c
@@ -46,25 +46,25 @@ static void submit_value (const char *type, const char *type_instance, value_t v
 }
 
 static void
-submit_percent(const char *type_instance, gauge_t percent)
+submit_percent(const char *metric_type, const char *type_instance, gauge_t percent)
 {
   value_t value;
 
   value.gauge = percent;
-  submit_value ("percent", type_instance, value);
+  submit_value (metric_type, type_instance, value);
 }
 
 static void
-submit_absolute(const char *type_instance, absolute_t absolute)
+submit_absolute(const char *metric_type, const char *type_instance, absolute_t absolute)
 {
   value_t value;
 
   value.absolute = absolute;
-  submit_value ("absolute", type_instance, value);
+  submit_value (metric_type, type_instance, value);
 }
 
 static void
-submit_derive(const char *type_instance, derive_t derive)
+submit_derive(const char *metric_type, const char *type_instance, derive_t derive)
 {
   value_t value;
 
@@ -83,15 +83,15 @@ static int sigar_read (void)
       sigar_cpu_perc_t perc;
       sigar_cpu_perc_calculate(&cpu_jiffles_snapshot, &cpu_jiffles_current, &perc);
 
-      submit_percent("system.cpu.user",      perc.user);
-      submit_percent("system.cpu.sys",       perc.sys);
-      submit_percent("system.cpu.nice",      perc.nice);
-      submit_percent("system.cpu.idle",      perc.idle);
-      submit_percent("system.cpu.wait",      perc.wait);
-      submit_percent("system.cpu.irq",       perc.irq);
-      submit_percent("system.cpu.soft_irq",  perc.soft_irq);
-      submit_percent("system.cpu.stolen",    perc.stolen);
-      submit_percent("system.cpu.combined",  perc.combined);
+      submit_percent("system.cpu", "user",      perc.user);
+      submit_percent("system.cpu", "sys",       perc.sys);
+      submit_percent("system.cpu", "nice",      perc.nice);
+      submit_percent("system.cpu", "idle",      perc.idle);
+      submit_percent("system.cpu", "wait",      perc.wait);
+      submit_percent("system.cpu", "irq",       perc.irq);
+      submit_percent("system.cpu", "soft_irq",  perc.soft_irq);
+      submit_percent("system.cpu", "stolen",    perc.stolen);
+      submit_percent("system.cpu", "combined",  perc.combined);
     } else {
       DEBUG ("sigar_cpu_get returned %i", res);
       return (-1);
@@ -101,14 +101,14 @@ static int sigar_read (void)
   {
     sigar_mem_t memory;
     if (SIGAR_OK == sigar_mem_get(sigar, &memory)) {
-      submit_absolute ("system.mem.ram",           memory.ram);
-      submit_absolute ("system.mem.total",         memory.total);
-      submit_absolute ("system.mem.used",          memory.used);
-      submit_absolute ("system.mem.free",          memory.free);
-      submit_absolute ("system.mem.actual_used",   memory.actual_used);
-      submit_absolute ("system.mem.actual_free",   memory.actual_free);
-      submit_percent  ("system.mem.used_percent",  memory.used_percent);
-      submit_percent  ("system.mem.free_percent",  memory.free_percent);
+      submit_absolute ("system.mem", "ram",           memory.ram);
+      submit_absolute ("system.mem", "total",         memory.total);
+      submit_absolute ("system.mem", "used",          memory.used);
+      submit_absolute ("system.mem", "free",          memory.free);
+      submit_absolute ("system.mem", "actual_used",   memory.actual_used);
+      submit_absolute ("system.mem", "actual_free",   memory.actual_free);
+      submit_percent  ("system.mem", "used_percent",  memory.used_percent);
+      submit_percent  ("system.mem", "free_percent",  memory.free_percent);
     } else {
       DEBUG ("sigar_mem_get returned %i", res);
       return (-1);
@@ -118,15 +118,35 @@ static int sigar_read (void)
   {
     sigar_net_stat_t network;
     if (SIGAR_OK == sigar_net_stat_get(sigar, &network, SIGAR_NETCONN_SERVER|SIGAR_NETCONN_TCP)) {
-      submit_absolute ("system.net.tcp_inbound_total",  network.tcp_inbound_total);
-      submit_absolute ("system.net.tcp_outbound_total", network.tcp_outbound_total);
-      submit_absolute ("system.net.all_inbound_total",  network.all_inbound_total);
-      submit_absolute ("system.net.all_outbound_total", network.all_outbound_total);
+      submit_absolute ("system.net", "tcp_inbound_total",  network.tcp_inbound_total);
+      submit_absolute ("system.net", "tcp_outbound_total", network.tcp_outbound_total);
+      submit_absolute ("system.net", "all_inbound_total",  network.all_inbound_total);
+      submit_absolute ("system.net", "all_outbound_total", network.all_outbound_total);
     } else {
       DEBUG ("sigar_mem_get returned %i", res);
       return (-1);
     }
   }
+
+  {
+    sigar_tcp_t tcp;
+    if (SIGAR_OK == sigar_tcp_get(sigar, &tcp)) {
+      submit_absolute ("system.tcp", "active_opens",   tcp.active_opens);
+      submit_absolute ("system.tcp", "passive_opens",  tcp.passive_opens);
+      submit_absolute ("system.tcp", "attempt_fails",  tcp.attempt_fails);
+      submit_absolute ("system.tcp", "estab_resets",   tcp.estab_resets);
+      submit_absolute ("system.tcp", "curr_estab",     tcp.curr_estab);
+      submit_absolute ("system.tcp", "in_segs",        tcp.in_segs);
+      submit_absolute ("system.tcp", "out_segs",       tcp.out_segs);
+      submit_absolute ("system.tcp", "retrans_segs",   tcp.retrans_segs);
+      submit_absolute ("system.tcp", "in_errs",        tcp.in_errs);
+      submit_absolute ("system.tcp", "out_rsts",       tcp.out_rsts);
+    } else {
+      DEBUG ("sigar_mem_get returned %i", res);
+      return (-1);
+    }
+  }
+
 
   return (0);
 }
